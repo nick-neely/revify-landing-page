e22907122f824a352fd4167e3f29d27a1b303a73
nick-neely <neelynickolas@gmail.com>
2024-10-26 20:03:57 -0500

    Update react-three-fiber types

--- src/types/react-three-fiber.d.ts
+++ src/types/react-three-fiber.d.ts
@@ -1,6 +1,15 @@
 import { ReactThreeFiber } from "@react-three/fiber";
+import * as THREE from "three";
 
 declare module "@react-three/fiber" {
   export const Canvas: ReactThreeFiber.Canvas;
-  export function useFrame(callback: (state: any, delta: number) => void): void;
+  export function useFrame(
+    callback: (state: RootState, delta: number) => void
+  ): void;
+
+  interface RootState {
+    clock: THREE.Clock;
+    camera: THREE.Camera;
+    scene: THREE.Scene;
+  }
 }
